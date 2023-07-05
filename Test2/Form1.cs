85dd11fbc437dd60854569cea6e389e7967d313f
azizmk12 <mohamedaziz.makri@esprit.tn>
2023-07-05 13:56:49 +0100

    first commit

---
+++ Test2/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace Test2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<Product> products = new List<Product>();
+            XmlSerializer searial = new XmlSerializer(typeof(List<Product>));
+            products.Add(new Product() {Id=1,Name="aziz"});
+            products.Add(new Product() {Id=2,Name="amir"});
+            using (FileStream fs = new FileStream(Path.Combine(Environment.CurrentDirectory, "Z:\\AZIZ\\4SAE\\stage4\\aziz.xml"), FileMode.Create, FileAccess.Write))
+            {
+                searial.Serialize(fs, products);
+                MessageBox.Show("Created");
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            List<Product> products = new List<Product>();
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                openFileDialog.Filter = "XML Files (*.xml)|*.xml";
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string filePath = openFileDialog.FileName;
+
+                    XmlSerializer serializer = new XmlSerializer(typeof(List<Product>));
+
+                    using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+                    {
+                        products = serializer.Deserialize(fs) as List<Product>;
+                    }
+                }
+            }
+
+            dataGridView1.DataSource = products;
+        }
+
+    }
+}
---
+++ Test2/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test2
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String LastName { get; set; }
+    }
+}
